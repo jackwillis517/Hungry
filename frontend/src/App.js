a78b9f8d2fd6824288dda80502cf5d1232606256
cwgurnha <cwgurnha@syr.edu>
2022-03-02 22:50:58 -0500

    AccountPage update
    Almost done with the skeleton

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login.js'
 import CreateUser from './pages/CreateUser.js'
 import Navbar from './pages/Navbar.js'
+import AccountPage from './pages/AccountPage.js' 
 
 const App = () => {
     return ( 
@@ -11,6 +12,7 @@ const App = () => {
                 <Route path="/login" element={<Login/>} />
                 <Route path="/createuser" element={<CreateUser/>} />
                 <Route path="/navbar" element={<Navbar/>} />
+                <Route path="/AccountPage" element={<AccountPage/>} />
             </Routes>
         </BrowserRouter>
     )
